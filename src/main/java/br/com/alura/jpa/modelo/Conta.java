fa17836dddbd84e69934adbf2ec59c18546cfced
Anderson Passos Silva <anderson.silva@zup.com.br>
2021-07-19 10:52:52 -0300

    Configura e Testa o Hibernate

---
+++ src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -0,0 +1,45 @@
+package br.com.alura.jpa.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Conta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+
+	private Integer agencia;
+
+	private Integer conta;
+
+	private String titular;
+
+	public Integer getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(Integer agencia) {
+		this.agencia = agencia;
+	}
+
+	public Integer getConta() {
+		return conta;
+	}
+
+	public void setConta(Integer conta) {
+		this.conta = conta;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+}
